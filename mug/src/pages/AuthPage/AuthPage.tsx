dad224fdc5b151e48203087274344405756b79f1
Artem <mr.pikless2@gmail.com>
2026-04-23 19:22:30 +0200

    Lol

--- mug/src/pages/AuthPage/AuthPage.tsx
+++ mug/src/pages/AuthPage/AuthPage.tsx
@@ -1,5 +1,13 @@
+import styles from "./AuthPage.module.css"
+
 export const AuthPage = () => {
     return(
-        <h1>Hallo frome Auth</h1>
-    );
+        <div className={styles.LogInrotationcontainer}>
+            <div className={styles.LogIncontainer}>
+                <input type="text" className={styles.LogInName} placeholder="Name"/><br/>
+                <input type="password" className={styles.Password} placeholder="Pass"/><br/>
+                <button className={styles.LogInbutton}>LogIn</button>
+            </div>
+        </div>
+    )
 };
