7e03e34f5a2f114ba32573b01548e27b83ced45f
Пенєв Семен Олександрович <roli.rp2@gmail.com>
2026-04-23 21:56:55 +0300

    added api for mug

--- mug/src/widgets/navbar/NavBar.tsx
+++ mug/src/widgets/navbar/NavBar.tsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
+import { api } from "../../app/api/api.ts";
 import styles from "./NavBar.module.css";
 
 export const NavBar = () => {
-        return(
-            <div className={styles.Nav}>
-                <Link to={"/"} className={styles.NavBTN}>Home</Link>
-                <Link to={"/Auth"} className={styles.NavBTN}>SignIn / SignUp</Link>
-            </div> 
-        );
-};
+  const isLoggedIn = api.auth.isLoggedIn();
 
+  function handleLogout() {
+    api.auth.logout();
+    window.location.href = "/";
+  }
+
+  return (
+    <div className={styles.Nav}>
+      <Link to={"/"} className={styles.NavBTN}>Home</Link>
+
+      {isLoggedIn ? (
+        <button className={styles.NavBTN} onClick={handleLogout}>
+          Logout
+        </button>
+      ) : (
+        <Link to={"/Auth"} className={styles.NavBTN}>SignIn / SignUp</Link>
+      )}
+    </div>
+  );
+};
